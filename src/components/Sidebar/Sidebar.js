5e30a7c8010144627530087abc99d459f33d8282
Robinkariuki <robinkaris7@gmail.com>
2020-10-16 19:21:07 +0300

    added sidebar css component

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './Sidebar.css';
 
 function Sidebar() {
     return (
-        <div>
+        <div className="sidebar">
             
         </div>
     )
 }
 
 export default Sidebar
+
+
