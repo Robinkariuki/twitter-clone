7bbf9c91b387e7fd003aa7c7210c72d2bcf641bd
Robinkariuki <robinkaris7@gmail.com>
2020-10-16 19:35:48 +0300

    added sidebar option component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import './App.css';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="app">
       <h1>twitter clone</h1>
+      <Sidebar/>
     </div>
   );
 }
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Sidebar.css';
+import HomeIcon from '@material-ui/icons/Home';
+import TwitterIcon from '@material-ui/icons/Twitter';
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            
+         <TwitterIcon/>
+         <HomeIcon/>
         </div>
     )
 }
