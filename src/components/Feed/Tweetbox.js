3eb8c61d771207cdab3111a484686e6622bf2bf0
Robinkariuki <robinkaris7@gmail.com>
2020-10-17 19:47:30 +0300

    made changes to tweetbox

--- src/components/Feed/Tweetbox.js
+++ src/components/Feed/Tweetbox.js
@@ -8,9 +8,12 @@ function Tweetbox() {
             <form>
                 <div className="tweetBox__input">
                     <Avatar src="https://www.denofgeek.com/wp-content/uploads/2017/03/darth-vader-1_0.jpg?resize=768%2C432"></Avatar>
+                    <input placeholder="what's happening?" type="text"></input>
                 </div>
-                <input placeholder="what's happening?"></input>
-                <Button>Tweet</Button>
+                <input className="tweetBox__imageInput" placeholder="OPtional:Enter image URL" type="text"></input>
+                
+
+                <Button className="tweetBox__tweetButton">Tweet</Button>
             </form>
         </div>
     )
