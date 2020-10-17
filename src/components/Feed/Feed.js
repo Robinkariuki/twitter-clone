a3ba6685bff15d082cef3cb4daefab27b9a2d679
Robinkariuki <robinkaris7@gmail.com>
2020-10-17 16:01:52 +0300

    made changes

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Feed.css';
+import Tweetbox from './Tweetbox';
 function Feed() {
     return (
         <div className="feed">
             <div className="feed__header">
             <h2>Home</h2>
             </div>
-           
+           <Tweetbox/>
         </div>
     )
 }
--- src/components/Feed/Tweetbox.js
+++ src/components/Feed/Tweetbox.js
@@ -1,10 +1,17 @@
+import { Button,Avatar } from '@material-ui/core';
 import React from 'react';
 import './Tweetbox.css';
 
 function Tweetbox() {
     return (
-        <div>
-            
+        <div className="tweetBox">
+            <form>
+                <div className="tweetBox__input">
+                    <Avatar src="https://www.denofgeek.com/wp-content/uploads/2017/03/darth-vader-1_0.jpg?resize=768%2C432"></Avatar>
+                </div>
+                <input placeholder="what's happening?"></input>
+                <Button>Tweet</Button>
+            </form>
         </div>
     )
 }
