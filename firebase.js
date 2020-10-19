da335e86c77aa75c4c1d579b8d474297a1ca854b
Robinkariuki <robinkaris7@gmail.com>
2020-10-19 19:27:57 +0300

    set up firebase backend

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyARILX6iiUT0N1OKA5huFLi1esfFZo-Ghk",
+    authDomain: "twitter-clone-e3dd5.firebaseapp.com",
+    databaseURL: "https://twitter-clone-e3dd5.firebaseio.com",
+    projectId: "twitter-clone-e3dd5",
+    storageBucket: "twitter-clone-e3dd5.appspot.com",
+    messagingSenderId: "914893608587",
+    appId: "1:914893608587:web:339e30a69feec879d3e68e",
+    measurementId: "G-S1SVD46L3G"
+  };
+
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig)
