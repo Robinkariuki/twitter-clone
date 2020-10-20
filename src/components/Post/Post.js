ee911cb2a72ff0ec86fe68817c0b190a1710cc06
Robinkariuki <robinkaris7@gmail.com>
2020-10-20 10:39:57 +0300

    made chages

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,5 +1,5 @@
 import { Avatar } from '@material-ui/core'
-import React from 'react'
+import React,{forwardRef} from 'react'
 import './Post.css'
 import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
@@ -8,44 +8,39 @@ import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import PublishIcon from '@material-ui/icons/Publish';
 import { VerifiedUser } from '@material-ui/icons';
 
-function Post({
-    displayName,
-    username,
-    verfied,
-    image,
-    text,
-    avatar
-}) {
-    return (
-        <div className="post">
-            <div className="post__avatar">
-                <Avatar src={avatar}></Avatar>
-
+const Post = forwardRef(
+    ({ displayName, username, verified, text, image, avatar }, ref) => {
+      return (
+        <div className="post" ref={ref}>
+          <div className="post__avatar">
+            <Avatar src={avatar} />
+          </div>
+          <div className="post__body">
+            <div className="post__header">
+              <div className="post__headerText">
+                <h3>
+                  {displayName}{" "}
+                  <span className="post__headerSpecial">
+                    {verified && <VerifiedUserIcon className="post__badge" />} @
+                    {username}
+                  </span>
+                </h3>
+              </div>
+              <div className="post__headerDescription">
+                <p>{text}</p>
+              </div>
             </div>
-        <div className="post__body">
-             <div className="post__header"> 
-             <div className="psot__headerText">
-                 <h3>
-    {displayName}{" "} <span className="post__headerSpecial"> 
-    { verfied && <VerifiedUserIcon className="post__badge"/>}@{username}
-                        </span> 
-                 </h3>
-             </div>
-             <div className="post__headerDescription">
-    <p>{text}</p> 
-
-             </div>
-             <img src={image} alt=""/>
-             <div className="post__footer">
-        <ChatBubbleOutlineIcon fontSize="small"/>
-        <RepeatIcon fontSize="small"/>
-        <FavoriteBorderIcon fontSize="small"/>
-        <PublishIcon fontSize="small"/>
-             </div>
-             </div>
+            <img src={image} alt="" />
+            <div className="post__footer">
+              <ChatBubbleOutlineIcon fontSize="small" />
+              <RepeatIcon fontSize="small" />
+              <FavoriteBorderIcon fontSize="small" />
+              <PublishIcon fontSize="small" />
+            </div>
+          </div>
         </div>
-        </div>
-    )
-}
-
-export default Post;
+      );
+    }
+  );
+  
+  export default Post;
