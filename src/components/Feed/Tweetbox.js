2a94d94f043bd6f9c7456273fd4d38442f1c6109
Robinkariuki <robinkaris7@gmail.com>
2020-10-20 10:17:09 +0300

    made chages

--- src/components/Feed/Tweetbox.js
+++ src/components/Feed/Tweetbox.js
@@ -1,19 +1,44 @@
 import { Button,Avatar } from '@material-ui/core';
-import React from 'react';
+import React,{useState} from 'react';
 import './Tweetbox.css';
-
+import db from '../../firebase';
 function Tweetbox() {
+    const [tweetMessage,setTweetMessage] = useState('')
+    const [tweetImage,setTweetImage] = useState('')
+
+   const sendTweet = (e) =>{
+       e.preventDefault();
+       
+       db.collection('Posts').add({
+           displayName:"dennis",
+           username:"hidra",
+           verified:true,
+           text:tweetMessage,
+           image:tweetImage,
+           avatar:"https://www.denofgeek.com/wp-content/uploads/2017/03/darth-vader-1_0.jpg?resize=768%2C432"
+
+    });
+    setTweetMessage("");
+    setTweetImage("");
+
+   } 
+
     return (
         <div className="tweetBox">
             <form>
                 <div className="tweetBox__input">
                     <Avatar src="https://www.denofgeek.com/wp-content/uploads/2017/03/darth-vader-1_0.jpg?resize=768%2C432"></Avatar>
-                    <input placeholder="what's happening?" type="text"></input>
+                    <input
+            onChange={(e) => setTweetMessage(e.target.value)}
+            value={tweetMessage}
+            placeholder="What's happening?"
+            type="text"
+          />
                 </div>
-                <input className="tweetBox__imageInput" placeholder="OPtional:Enter image URL" type="text"></input>
+                <input  onChange={(e)=>setTweetImage(e.target.value)}   value={tweetImage} className="tweetBox__imageInput" placeholder="OPtional:Enter image URL" type="text"></input>
                 
 
-                <Button className="tweetBox__tweetButton">Tweet</Button>
+                <Button onClick={sendTweet} type="submit" className="tweetBox__tweetButton">Tweet</Button>
             </form>
         </div>
     )
